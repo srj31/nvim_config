37725f8fe6b1e7ca3efbcdbc10d27d1d87f15d56
Sourabh Jaiswal <sourabhrj31@gmail.com>
2026-06-23 02:26:51 +0800

    feat: vim-illuminate symbol references

---
+++ lua/srj31/plugins/illuminate.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "RRethy/vim-illuminate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = { providers = { "lsp", "treesitter", "regex" }, delay = 120 },
+    config = function(_, opts)
+      require("illuminate").configure(opts)
+    end,
+  },
+}
