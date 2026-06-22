6529ea43c373e5a5d53979417e9e0a489325b991
Sourabh Jaiswal <sourabhrj31@gmail.com>
2026-06-23 02:08:52 +0800

    fix: pin nvim-treesitter to master branch

--- lua/srj31/plugins/treesitter.lua
+++ lua/srj31/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     main = "nvim-treesitter.configs",
