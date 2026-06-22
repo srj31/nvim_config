6dd33ec43a03716a0823ad695f15f027d681017c
Sourabh Jaiswal <sourabhrj31@gmail.com>
2026-06-23 02:55:11 +0800

    feat: treesitter textobjects

--- lua/srj31/plugins/treesitter.lua
+++ lua/srj31/plugins/treesitter.lua
@@ -3,6 +3,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     branch = "master",
     build = ":TSUpdate",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
+    },
     event = { "BufReadPost", "BufNewFile" },
     main = "nvim-treesitter.configs",
     opts = {
@@ -16,6 +19,26 @@ return {
       auto_install = true,
       highlight = { enable = true, additional_vim_regex_highlighting = false },
       indent = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = { ["]f"] = "@function.outer", ["]a"] = "@parameter.inner" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[a"] = "@parameter.inner" },
+        },
+      },
     },
   },
 }
