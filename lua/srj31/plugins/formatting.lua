cc359195367c785dc55dcb947c38492a397f2766
Sourabh Jaiswal <sourabhrj31@gmail.com>
2026-06-23 01:03:21 +0800

    feat: conform.nvim formatting + nvim-lint (replaces null-ls)

---
+++ lua/srj31/plugins/formatting.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>fm",
+        function() require("conform").format({ async = true, lsp_format = "fallback" }) end,
+        mode = { "n", "v" },
+        desc = "Format buffer",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        markdown = { "prettier" },
+        ocaml = { "ocamlformat" },
+        haskell = { "fourmolu" },
+        sh = { "shfmt" },
+        python = { "ruff_format" },
+        lua = { "stylua" },
+      },
+      format_on_save = { timeout_ms = 1000, lsp_format = "fallback" },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = { sh = { "shellcheck" } }
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+        group = vim.api.nvim_create_augroup("srj31_lint", { clear = true }),
+        callback = function() lint.try_lint() end,
+      })
+    end,
+  },
+}
