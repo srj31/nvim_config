c4f43226f0da4bfbbac5ac014165f28bc54b4486
Sourabh Jaiswal <sourabhrj31@gmail.com>
2026-06-23 02:08:58 +0800

    fix: add roslyn custom mason registry

--- lua/srj31/plugins/lsp.lua
+++ lua/srj31/plugins/lsp.lua
@@ -3,7 +3,15 @@ return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
+      {
+        "mason-org/mason.nvim",
+        opts = {
+          registries = {
+            "github:mason-org/mason-registry",
+            "github:Crashdummyy/mason-registry",
+          },
+        },
+      },
       "mason-org/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
       "saghen/blink.cmp",
